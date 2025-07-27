799171c1508896a18cc3871511fb0c79c80fee7e
th-sakib <mdthamidulhasansakib@gmail.com>
2025-07-27 16:28:40 +0600

    Add array buffer writing program

--- 000-NODEJS/09_Mastering_Buffers/01_ArrayBuffer/app.js
+++ 000-NODEJS/09_Mastering_Buffers/01_ArrayBuffer/app.js
@@ -12,7 +12,23 @@ view.setInt8(2, 0o20); // store octal value 80 into the buffer
 view.setInt8(3, 0b101010); // store binary value 80 into the buffer
 
 // reading value in ArrayBuffer using the view.
-console.log(view.getInt8(0)) // 90 
-console.log(view.getInt8(1)) // 80
-console.log(view.getInt8(2)) // 16
-console.log(view.getInt8(3)) // 42
+// console.log(view.getInt8(0)) // 90 
+// console.log(view.getInt8(1)) // 80
+// console.log(view.getInt8(2)) // 16
+// console.log(view.getInt8(3)) // 42
+
+// ---
+const buffer2 = new ArrayBuffer(4);
+const view2 = new DataView(buffer2);
+
+view2.setInt8(0, 255);
+console.log(view2.getInt8(0)); // -1 | getInt8 and setInt8. sets and gets value as SIGNED value.
+
+console.log(view2.getUint8(0)); // 255 | getUint8 gets value as UNSIGNED value.
+
+view2.setUint8(1, 255);
+console.log(view2.getUint8(1)); // 255 | getUint8 and setUInt8. sets and gets value as UnSIGNED value.
+
+view2.setInt8(1, 255);
+console.log(view2.getUint8(1)); // 255 | getUint8 and setUInt8. sets and gets value as UnSIGNED value.
+console.log(view2);
